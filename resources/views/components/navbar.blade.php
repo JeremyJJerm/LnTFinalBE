9d9415e7cc228438671a035dbb3d6947ff336f11
JeremyJJerm <159779674+JeremyJJerm@users.noreply.github.com>
2024-05-06 01:11:14 +0700

    First Commit

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,7 @@
 <div>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-gradient" style="background-color: #edf6fc;">
         <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">ChipiChapa</span>
           <a class="navbar-brand" href="/login">Login</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
